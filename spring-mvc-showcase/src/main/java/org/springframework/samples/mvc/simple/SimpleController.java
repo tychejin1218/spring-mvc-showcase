7e9dda0897c8686e9c3cc1b6ab087e150d2566d3
tychejin1218 <tychejin@naver.com>
2020-04-15 23:27:38 +0900

    [Spring-MVC-ShowCase] logger 확인

--- spring-mvc-showcase/src/main/java/org/springframework/samples/mvc/simple/SimpleController.java
+++ spring-mvc-showcase/src/main/java/org/springframework/samples/mvc/simple/SimpleController.java
@@ -1,13 +1,18 @@
 package org.springframework.samples.mvc.simple;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
 
+	private static final Logger logger = LoggerFactory.getLogger(SimpleController.class);
+	
 	@GetMapping("/simple")
 	public String simple() {
+		logger.info("SimpleController.simple");
 		return "Hello world!";
 	}
 }
